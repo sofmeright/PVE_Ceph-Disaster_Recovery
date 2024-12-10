ac8f5cadb62ccc8192a35e4d223f40c2918ae54b
SoFMeRight <sofmeright@gmail.com>
2024-12-10 00:32:36 -0800

    Just a routine update...

--- recover_monstore_from_osds-bluestore.sh
+++ recover_monstore_from_osds-bluestore.sh
@@ -4,12 +4,14 @@
 set -xe
 
 # CHANGE These!
+# If all your hosts contain osds no need to define them all. Import them dynamically.
+hosts_auto_populate=0
 # Hosts that provide OSDs - if you don't specify a host here that has OSDs, they will become "Ghost OSDs" in rebuild and data may be lost
 hosts=( "Avocado" "Bamboo" "Cosmos" )
 # Your rebuild target path. Make sure it's large enough and empty (couple of GB for a big cluster, not the sum of OSD size)
 ms=/root/mon-store
 # set to 1 if the osds are bluestore otherwise set to 0.
-bluestore_bool=1
+bluestore_flag=1
 
 # FUNCTION DEFINITIONS ~ Feel free to read to see how I implemented these as you see it below in the main runtime.
 function osd_get_block_device() { # syntax pass osd #
@@ -27,7 +29,34 @@ function osd_mount_bluestore() { # syntax pass osd #
     ln -snf $(osd_get_block_device $1) /var/lib/ceph/osd/ceph-$1/block
 }
 function mon_rebuild_from_osd() {
-    ceph-objectstore-tool --type bluestore --data-path \$1 --no-mon-config --op update-mon-db --mon-store-path $ms.remote || true
+    if [ $bluestore_flag -ne 1 ]; then
+        ceph-objectstore-tool --type bluestore --data-path \$1 --no-mon-config --op update-mon-db --mon-store-path $ms.remote || true
+        else ceph-objectstore-tool --data-path \$1 --no-mon-config --op update-mon-db --mon-store-path $ms.remote || true
+        fi
+}
+function hosts_get_names() {
+    awk '
+    # Flag to track when inside a node block
+    /node {/ {
+        in_node = 1;  # Set the flag when "node {" is found
+    }
+
+    # Look for "name:" within a node block
+    in_node && /name:/ {
+        # Split the line on colon to get the name
+        split($0, parts, ":");
+        # Trim any leading/trailing whitespace and print the name
+        gsub(/^[ \t]+|[ \t]+$/, "", parts[2]);
+        print parts[2];
+    }
+
+    # End of a node block
+    /}/ {
+        if (in_node) {
+            in_node = 0;  # Reset the flag
+        }
+    }
+' "$1"
 }
 # --------------------------------------------------------------------------------------------------------------------------
 
@@ -35,9 +64,10 @@ function mon_rebuild_from_osd() {
 # if there is a monstore back it up!
 
 #remove the monmap
-rm -r $ms || true
-mkdir $ms || true
+#rm -r $ms || true
+#mkdir $ms || true
 
 osd_get_block_device 0
 osd_get_mount_path 0
 osd_mount_bluestore 0
+hosts_get_names /etc/pve/corosync.conf
