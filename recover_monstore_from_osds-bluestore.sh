b17f8df4d9d1362282d54a43a006d7aa6c318ac4
Kai <kai.hamil@gmail.com>
2024-12-10 03:03:48 -0800

    This is a completed script... Testing. Until I get confirmation this works I do not vet this. However if you are an experienced user feel free to read through this and use if you trust my code.

--- recover_monstore_from_osds-bluestore.sh
+++ recover_monstore_from_osds-bluestore.sh
@@ -3,6 +3,9 @@
 # set -e ~ If the return code of one command is not 0 and the caller does not check it, the shell script will exit. This feature make shell script robust.
 set -xe
 
+# Notes: This may work for other OS than PVE, you may want to tweak the mgr get key function if the mgr key paths will be different in that case. There may be other paths called dynamically in the function calls. 
+# This is meant to be a for dummies rescue script for PVE so it's limited what I am adding to the script here. Other than that, everything else is configurable here afaik.
+
 # CHANGE These!
 # If all your hosts contain osds no need to define them all. Import them dynamically.
 hosts_auto_populate=0
@@ -12,6 +15,8 @@ hosts=( "Avocado" "Bamboo" "Cosmos" )
 ms=/root/mon-store
 # set to 1 if the osds are bluestore otherwise set to 0.
 bluestore_flag=1
+# You probably need this mon key path if you are using proxmox
+KEYRING="/etc/pve/priv/ceph.mon.keyring"
 
 # FUNCTION DEFINITIONS ~ Feel free to read to see how I implemented these as you see it below in the main runtime.
 function osd_get_block_device() { # syntax pass osd #
@@ -25,8 +30,28 @@ function osd_get_mount_path() { # syntax pass osd #
     echo /var/lib/ceph/osd/ceph-$1/block
 }
 function osd_mount_bluestore() { # syntax pass osd #
-    ceph-bluestore-tool --cluster=ceph prime-osd-dir --dev $(osd_get_block_device $1) --path /var/lib/ceph/osd/ceph-$1
-    ln -snf $(osd_get_block_device $1) /var/lib/ceph/osd/ceph-$1/block
+    if [ $bluestore_flag -ne 1 ]; then
+        ceph-bluestore-tool --cluster=ceph prime-osd-dir --dev $(osd_get_block_device $1) --path /var/lib/ceph/osd/ceph-$1
+        ln -snf $(osd_get_block_device $1) /var/lib/ceph/osd/ceph-$1/block
+        fi
+}
+function mgr_get_key() {
+    # Temporarily store a copy of the mgr keyring from the remote node to gain a copy of the key.
+    scp root@$1:/var/lib/ceph/mgr/ceph-$1/keyring /root/mgr-ceph-$1-keyring
+    local file="/root/mgr-ceph-$1-keyring"
+    # Extract the key using awk
+    local key=$(awk -F '=' '/key *=/ {
+        # Trim leading/trailing whitespace from the key value
+        gsub(/^[ \t]+|[ \t]+$/, "", $2);
+        if (length($2) > 0) {
+            print $2;
+            exit;
+        }
+    }' "$file")
+    # Remove the file
+    rm "$file"
+    # Return the extracted key
+    echo "$key"
 }
 function mon_rebuild_from_osd() {
     if [ $bluestore_flag -ne 1 ]; then
@@ -74,14 +99,72 @@ function hosts_get_names() {
 }
 # --------------------------------------------------------------------------------------------------------------------------
 
+if [ $hosts_auto_populate -ne 1 ]; then
+        hosts=$hosts_get_names
+        fi
 
-# if there is a monstore back it up!
+# Ensure the folder we use as a target for the monmap rebuild starts empty.
+rm -r $ms || true
+mkdir $ms || true
 
-#remove the monmap
-#rm -r $ms || true
-#mkdir $ms || true
+# collect the cluster map from stopped OSDs - basically, this daisy-chains the gathering. Make
+# sure to start with clean folders, or the rebuild will fail when starting ceph-mon
+# (update_from_paxos assertion error) (the rm -rf is no mistake here)
+for host in "${hosts[@]}"; do
+    rsync -avz $ms/. root@$host:$ms.remote
+    rm -rf $ms
+    ssh root@$host <<EOF
+        set -x
+        for osd in /var/lib/ceph/osd/ceph-*; do
+            # We do need the || true here to not crash when ceph tries to recover the osd-{node}-Directory present on some hosts
+            osd_mount_bluestore $osd
+            mon_rebuild_from_osd $osd
+            done
+        EOF
+    rsync -avz --remove-source-files root@$host:$ms.remote/. $ms
+    done
 
-osd_get_block_device 0
-osd_get_mount_path 0
-osd_mount_bluestore 0
-hosts_get_names
+# rebuild the monitor store from the collected map, if the cluster does not
+# use cephx authentication, we can skip the following steps to update the
+# keyring with the caps, and there is no need to pass the "--keyring" option.
+# i.e. just use "ceph-monstore-tool $ms rebuild" instead
+ceph-authtool "$KEYRING" -n mon. \
+  --cap mon 'allow *'
+ceph-authtool "$KEYRING" -n client.admin \
+  --cap mon 'allow *' --cap osd 'allow *' --cap mds 'allow *'
+# add one or more ceph-mgr's key to the keyring. in this case, an encoded key
+# for mgr.x is added, you can find the encoded key in
+# /etc/ceph/${cluster}.${mgr_name}.keyring on the machine where ceph-mgr is
+# deployed
+
+for host in "${hosts[@]}"; do
+    ceph-authtool "$KEYRING" --add-key '$(mgr_get_key $host)' -n mgr.$host \
+        --cap mon 'allow profile mgr' --cap osd 'allow *' --cap mds 'allow *'
+    done
+
+# If your monitors' ids are not sorted by ip address, please specify them in order.
+# For example. if mon 'a' is 10.0.0.3, mon 'b' is 10.0.0.2, and mon 'c' is  10.0.0.4,
+# please passing "--mon-ids b a c".
+# In addition, if your monitors' ids are not single characters like 'a', 'b', 'c', please
+# specify them in the command line by passing them as arguments of the "--mon-ids"
+# option. if you are not sure, please check your ceph.conf to see if there is any
+# sections named like '[mon.foo]'. don't pass the "--mon-ids" option, if you are
+# using DNS SRV for looking up monitors.
+# This will fail if the provided monitors are not in the ceph.conf or if there is a mismatch in length. SET YOUR OWN monitor IDs here
+ceph-monstore-tool $ms rebuild -- --keyring "$KEYRING" --mon-ids $hosts_get_names
+
+# make a backup of the existing store.db just in case!  repeat for all monitors.
+# CAREFUL here: Running the script multiple times will never overwrite the *original* backup! (Cause that's unsafe imho)
+
+for host in "${hosts[@]}"; do
+    # if there is not an original backup db in the folder back it up.
+    if ssh "root"@"$host" "[ ! -e /var/lib/ceph/mon/ceph-$host/store.db.original ]"; then
+        ssh root@$host "mv /var/lib/ceph/mon/ceph-$host/store.db /var/lib/ceph/mon/ceph-$host/store.db.original"
+        else 
+            ssh root@$host "rm -r /var/lib/ceph/mon/ceph-$host/store.db.bak
+            ssh root@$host "mv /var/lib/ceph/mon/ceph-$host/store.db /var/lib/ceph/mon/ceph-$host/store.db.bak"
+        fi
+        # move rebuild store.db into place.  repeat for all monitors.
+        rsync -av $ms/store.db root@$host:/var/lib/ceph/mon/ceph-$host/store.db
+        ssh root@$host "chown -R ceph:ceph /var/lib/ceph/mon/ceph-$host/store.db"
+    done
